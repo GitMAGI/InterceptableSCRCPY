a391e4002a66addec57b7c178068922659e54739
Leonardo Paglionico <leonardo.paglionico@dxc.com>
2019-04-03 12:27:57 +0200

    Commit Inizializzazione

---
+++ src/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define DEFAULT_LOCAL_PORT 27183
+#define SCRCPY_VERSION "x.0.0"
+#define DEFAULT_MAX_SIZE 0
+#define DEFAULT_BIT_RATE 8000000
+#define PREFIX ""
+#define PREFIXED_SERVER_PATH "/share/scrcpy/scrcpy-server.jar"
+
+#endif
